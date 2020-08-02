0655c6eb1752b1de17d73110a606f9745552af00
Haruka Aoki <drgripa1@gmail.com>
2020-08-03 01:13:56 +0900

    fix option

--- options.py
+++ options.py
@@ -19,7 +19,7 @@ def parse_args_train():
     parser.add_argument('--weight_decay', help='optimizer weight decay (L2 reg.)', type=float, default=0.0001)
     parser.add_argument('--decay_lr_1', help='iteration at which lr decays 1st', type=int, default=32000)
     parser.add_argument('--decay_lr_2', help='iteration at which lr decays 2nd', type=int, default=48000)
-    parser.add_argument('--lr_decay_rate', help='lr *= lr_decay_rate at drop_lr_i-th iteration', type=float, default=0.1)
+    parser.add_argument('--lr_decay_rate', help='lr *= lr_decay_rate at decay_lr_i-th iteration', type=float, default=0.1)
     parser.add_argument('--n_iter', help='learning iterations', type=int, default=64000)
     args = parser.parse_args()
 
